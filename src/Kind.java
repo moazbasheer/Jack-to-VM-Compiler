3cfb0f615e7aa606a7d2270273b7945a3f6ab1d5
moazbasheer <moazbasheer97@gmail.com>
2020-07-18 12:17:10 +0200

    added the kinds of attributes in the class.

---
+++ src/Kind.java
@@ -0,0 +1,7 @@
+public class Kind {
+    public static final int NONE = -1;
+    public static final int STATIC = 0;
+    public static final int FIELD = 1;
+    public static final int ARG = 2;
+    public static final int VAR = 4;
+}
